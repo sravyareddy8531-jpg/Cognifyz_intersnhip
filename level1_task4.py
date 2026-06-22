3e7cffb7bc1750acaa7c03602e2ecbed68048f9c
sravyareddy8531-jpg <sravyareddy8531@gmail.com>
2026-06-22 16:17:28 +0530

    Add files via upload

---
+++ level1_task4.py
@@ -0,0 +1,26 @@
+def calculator():
+    print("--- Simple Calculator ---")
+    
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /, %): ")
+    num2 = float(input("Enter second number: "))
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Error! Division by zero."
+    elif operator == '%':
+        result = num1 % num2
+    else:
+        return "Invalid Operator!"
+
+    return f"Result: {result}"
+
+print(calculator())
